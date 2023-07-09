55c6baaf535565c85f35686a73c8cfdf9e01ca27
Martijn Dormans <martijn.dormans@gmail.com>
2023-07-09 14:37:08 +0200

    check if variables are working

--- ci/build-prod-env.sh
+++ ci/build-prod-env.sh
@@ -19,6 +19,6 @@ sed -i 's~%DATABASE_NAME%~'"$DATABASE_NAME"'~' .env
 sed -i 's~%DATABASE_USERNAME%~'"$DATABASE_USERNAME"'~' .env
 sed -i 's~%DATABASE_PASSWORD%~'"$DATABASE_PASSWORD"'~' .env
 
-echo "$(<.env)"
+echo $DATABASE_HOST
 
 printf "Done... \n"

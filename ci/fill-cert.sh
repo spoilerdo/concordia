5871a934152aea240092042b61048d4b74022374
Martijn Dormans <martijn.dormans@gmail.com>
2023-07-09 12:41:43 +0200

    Fix fill cert path mismatch

--- ci/fill-cert.sh
+++ ci/fill-cert.sh
@@ -2,6 +2,8 @@
 #stops execution if script has an error
 set -e
 
+cd ci/
+
 printf "Filling in SSL CA Certification for Postgress DB connection... \n"
 
 sed -i 's~%DATABASE_CRT%~'"$DATABASE_CRT"'~' azureDbCA.crt.pem

bddb733bca15af26185c1a66c42cb7fddffade7c
Martijn Dormans <martijn.dormans@gmail.com>
2023-07-16 09:51:05 +0200

    Remove default variables and update pipeline with env variables from github

--- config/database.ts
+++ config/database.ts
@@ -58,11 +58,11 @@ export default ({ env }) => {
     postgres: {
       connection: {
         connectionString: env("DATABASE_URL"),
-        host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "strapi"),
-        user: env("DATABASE_USERNAME", "strapi"),
-        password: env("DATABASE_PASSWORD", "strapi"),
+        host: env("DATABASE_HOST"),
+        port: env.int("DATABASE_PORT"),
+        database: env("DATABASE_NAME"),
+        user: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
         ssl:
           env.bool("DATABASE_SSL") === true
             ? {
